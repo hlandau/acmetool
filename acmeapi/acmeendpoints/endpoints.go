3bcfa1efab009533429052e4490db56e0ea6be19
Hugo Landau <hlandau@devever.net>
2017-06-04 18:44:14 +0100

    Fix OCSP URL regexps for Let's Encrypt.
    ©! I, Hugo Landau <hlandau@devever.net>, hereby licence these changes under the
    ©! licence with SHA256 hash
    ©! fd80a26fbb3f644af1fa994134446702932968519797227e07a1368dea80f0bc.

--- acmeapi/acmeendpoints/endpoints.go
+++ acmeapi/acmeendpoints/endpoints.go
@@ -6,7 +6,7 @@ var (
 		Code:                   "LetsEncryptLive",
 		Title:                  "Let's Encrypt (Live)",
 		DirectoryURL:           "https://acme-v01.api.letsencrypt.org/directory",
-		OCSPURLRegexp:          `^http://ocsp\.int-[^.]+\.letsencrypt\.org\.?/.*$`,
+		OCSPURLRegexp:          `^http://ocsp\.int-[^.]+\.letsencrypt\.org\.?(/.*)?$`,
 		CertificateURLRegexp:   `^https://acme-v01\.api\.letsencrypt\.org\.?/acme/cert/.*$`,
 		CertificateURLTemplate: `https://acme-v01.api.letsencrypt.org/acme/cert/{{.Certificate.SerialNumber|printf "%036x"}}`,
 	}
@@ -16,7 +16,7 @@ var (
 		Code:                   "LetsEncryptStaging",
 		Title:                  "Let's Encrypt (Staging)",
 		DirectoryURL:           "https://acme-staging.api.letsencrypt.org/directory",
-		OCSPURLRegexp:          `^http://ocsp\.(staging|stg-int)-[^.]+\.letsencrypt\.org\.?/.*$`,
+		OCSPURLRegexp:          `^http://ocsp\.(staging|stg-int)-[^.]+\.letsencrypt\.org\.?(/.*)?$`,
 		CertificateURLRegexp:   `^https://acme-staging\.api\.letsencrypt\.org\.?/acme/cert/.*$`,
 		CertificateURLTemplate: `https://acme-staging.api.letsencrypt.org/acme/cert/{{.Certificate.SerialNumber|printf "%036x"}}`,
 	}
