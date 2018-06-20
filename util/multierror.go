f4e7668c0a0968b16f53779fc26580a23dffa4a1
Hugo Landau <hlandau@devever.net>
2018-06-20 21:14:36 +0100

    Add error handling utilities
    ©! I, Hugo Landau <hlandau@devever.net>, hereby licence these changes under the
    ©! licence with SHA256 hash
    ©! fd80a26fbb3f644af1fa994134446702932968519797227e07a1368dea80f0bc.

---
+++ util/multierror.go
@@ -0,0 +1,65 @@
+package util
+
+import "fmt"
+
+// Used to return multiple errors, for example when several targets cannot be
+// reconciled. This prevents one failing target from blocking others.
+type MultiError []error
+
+func (merr MultiError) Error() string {
+	s := ""
+	for _, e := range merr {
+		if s != "" {
+			s += "; \n"
+		}
+		s += e.Error()
+	}
+	return "the following errors occurred:\n" + s
+}
+
+// Used to return an error that wraps another error.
+type WrapError struct {
+	Msg string
+	Sub error
+}
+
+func NewWrapError(sub error, msg string, args ...interface{}) *WrapError {
+	return &WrapError{
+		Msg: fmt.Sprintf(msg, args...),
+		Sub: sub,
+	}
+}
+
+func (werr *WrapError) Error() string {
+	return fmt.Sprintf("%s [due to inner error: %v]", werr.Msg, werr.Sub)
+}
+
+// PertError knows whether it's temporary or not.
+type PertError struct {
+	error
+	temporary bool
+}
+
+func NewPertError(isTemporary bool, sub error) error {
+	return &PertError{sub, isTemporary}
+}
+
+func (e *PertError) Temporary() bool {
+	return e.temporary
+}
+
+type tmp interface {
+	Temporary() bool
+}
+
+// Returns whether an error is temporary or not. An error is temporary if it
+// implements the interface { Temporary() bool } and that method returns true.
+// Errors which don't implement this interface aren't temporary.
+func IsTemporary(err error) bool {
+	x, ok := err.(tmp)
+	if !ok {
+		return false
+	}
+
+	return x.Temporary()
+}
